024b96e73212dab039e29cc8f00ac6c092f2d172
Amit Bora <info.amitsinghbora@gmail.com>
2025-03-21 05:49:00 +0000

    feat: add execute action

---
+++ src/core/executeAction.ts
@@ -0,0 +1,37 @@
+import { IRead, IHttp } from "@rocket.chat/apps-engine/definition/accessors";
+import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
+import { IExecutionContext } from "../definations/IExecutionContext";
+import { EmailBridgeApp } from "../../EmailBridgeApp";
+
+export async function executeAction(
+    app: EmailBridgeApp,
+    user: IUser,
+    room: IRoom,
+    read: IRead,
+    http: IHttp,
+    executionContext: IExecutionContext
+) {
+    for (const actionId of executionContext.actionIds) {
+        switch (actionId) {
+            case "summary":
+                console.log("Summary action triggered");
+                break;
+            case "send-email":
+                console.log("Send email action triggered");
+                break;
+            case "search-email":
+                console.log("Search email action triggered");
+                break;
+            case "send-message":
+                console.log("Send message action triggered");
+                break;
+            case "report":
+                console.log("Report action triggered");
+                break;
+            default:
+                console.warn(`Unknown action type: ${actionId}`);
+                break;
+        }
+    }
+}
